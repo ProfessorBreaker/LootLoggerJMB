40ff4dcf4bf9abf779d02e75da94be3ffcc68f26
Professor Breaker <jbreak1@ju.edu>
2025-10-09 15:41:10 -0400

    Step 1

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -8,5 +8,38 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+
+        }
+
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+
+        }
+    
+    //Set number of Row in cell
+    override func tableView(_ tableView: UITableView,
+            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a new or recycled cell
+           let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+               for: indexPath)
+
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+
+        return cell
+    }
 
 }
